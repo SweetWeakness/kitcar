538d565731890ba5ce10b8b2cf4342aec9b46278
sweetweakness <puri4y@gmail.com>
2025-05-04 01:59:20 +0300

    fix3

--- app/services/images/endpoints.py
+++ app/services/images/endpoints.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, Query
-from typing import List, Optional, Dict, Union
+from fastapi import APIRouter, UploadFile, File
+import shutil
+import os
 from pydantic import BaseModel
 from fastapi.responses import FileResponse
-import datetime
+import uuid
 
 from app.utils import replies_db
 
@@ -32,3 +33,17 @@ def get_rental_filter_options(fdir: str, file_name: str):
     Получение картинки с backend'а в конкретной директории
     """
     return FileResponse(path="./data/%s/%s" % (fdir, str(file_name)))
+
+
+@router.post("/upload_image")
+async def upload_file(file: UploadFile = File(...)):
+    """
+    Загрузка файла на сервер в папку /uploads
+    """
+    ext = os.path.splitext(file.filename)[-1]  # сохраняем расширение
+    unique_name = f"{uuid.uuid4()}{ext}"
+    file_location = os.path.join(os.getcwd(), "data", "temp", unique_name)
+    with open(file_location, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    return {"location": f"/images/temp/{unique_name}"}
